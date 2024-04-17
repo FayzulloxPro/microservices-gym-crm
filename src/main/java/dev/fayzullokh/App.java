6832b1a2c3abfb67a7c1e34ca15eb4ee57856072
Fayzullox <qahramonovf26@gmail.com>
2024-04-17 19:23:50 +0500

    commit

--- src/main/java/dev/fayzullokh/App.java
+++ src/main/java/dev/fayzullokh/App.java
@@ -4,13 +4,11 @@ package dev.fayzullokh;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @OpenAPIDefinition
-@EnableDiscoveryClient
 @EnableJpaRepositories
 @EnableMongoRepositories
 public class App {
